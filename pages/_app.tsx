91357ff926769f9c7e09e1a631f3367f0d0246a3
Lordwill Mabalot <lordwill.dev@gmail.com>
2023-04-11 21:08:41 +0800

    Set up mantine ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,34 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Poppins } from 'next/font/google';
+import { MantineProvider } from '@mantine/core';
+
+const poppins = Poppins(
+  {
+    subsets: ['latin'],
+    weight: ['300', '400', '500'],
+  },
+);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'light',
+          fontFamily: poppins.style.fontFamily,
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
